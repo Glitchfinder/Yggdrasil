f719b91430bf48542139631c1fdf9ce7fb88dfce
Glitchfinder <glitchkey@gmail.com>
2016-12-23 01:20:45 -0800

    Initial commit

---
+++ src/main/java/com/glitchkey/yggdrasil/Yggdrasil.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2016 Sean Porter <glitchkey@gmail.com>
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.glitchkey.yggdrasil;
+
+//* IMPORTS: JDK/JRE
+	//* NOT NEEDED
+//* IMPORTS: SPONGE
+import org.spongepowered.api.Game;
+import org.spongepowered.api.event.game.state.GamePreInitializationEvent;
+import org.spongepowered.api.event.game.state.GameStoppingServerEvent;
+import org.spongepowered.api.plugin.Plugin;
+import org.spongepowered.api.config.DefaultConfig;
+import org.spongepowered.api.event.Listener;
+//* IMPORTS: YGGDRASIL
+	//* NOT NEEDED
+//* IMPORTS: OTHER
+import com.google.inject.Inject;
+import ninja.leaping.configurate.commented.CommentedConfigurationNode;
+import ninja.leaping.configurate.loader.ConfigurationLoader;
+import org.slf4j.Logger;
+
+/**
+ * A simple sponge plugin
+ */
+@Plugin(id = PomData.ARTIFACT_ID, name = PomData.NAME, version = PomData.VERSION)
+public class Yggdrasil {
+
+    // These are all injected on plugin load for users to work from
+    @Inject private Logger logger;
+    // Give us a configuration to work from
+    @Inject @DefaultConfig(sharedRoot = true) private ConfigurationLoader<CommentedConfigurationNode> configLoader;
+    @Inject private Game game;
+
+    @Listener
+    public void onPreInit(GamePreInitializationEvent event) {
+        // Perform initialization tasks here
+    }
+
+    @Listener
+    public void disable(GameStoppingServerEvent event) {
+        // Perform shutdown tasks here
+    }
+}
